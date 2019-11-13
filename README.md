d4e7322037a7d912278deb6ed96af442a91ffb9a
jamesdobson <dobson@softwarepunk.com>
2019-11-13 00:35:34 -0500

    🎉 Hello, Gitmoji

---
+++ README.md
@@ -0,0 +1,16 @@
+# gogitmoji
+
+[Gitmoji](https://gitmoji.carloscuesta.me/) helper written with ❤️ in Go. Inspired by [gitmoji-cli](https://github.com/carloscuesta/gitmoji-cli).
+
+Currently under construction 🚧, not much is implemented!
+
+## Capabilities
+- [x] Select a Gitmoji from a list
+- [ ] Search name, description, and code
+- [ ] Automatically get latest Gitmoji database
+- [ ] Ask full list of Gitmoji commit message questions and output commit message
+- [ ] Implement first automated tests
+- [ ] Commit to git with commit message
+- [ ] Autobuilds and releases
+- [ ] Install git commit hook
+- [ ] Support conventional commits (with gitmoji)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+)
+
+type GitmojiContainer struct {
+	Gitmoji []Gitmoji `json:"gitmojis"`
+}
+
+type Gitmoji struct {
+	Emoji       string
+	Entity      string
+	Code        string
+	Description string
+	Name        string
+}
+
+func main() {
+	file, _ := ioutil.ReadFile("gitmojis.json")
+	gitmojiContainer := GitmojiContainer{}
+	_ = json.Unmarshal([]byte(file), &gitmojiContainer)
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   "‣ {{ .Emoji }} {{ .Code | cyan }} {{ .Description }}",
+		Inactive: "  {{ .Emoji }} {{ .Code | cyan }} {{ .Description }}",
+		Selected: "{{ .Emoji }} {{ .Code | cyan }} {{ .Description }}",
+		Details: `
+--------- Gitmoji ----------
+{{ "Name:" | faint }}	{{ .Emoji }} {{ .Name }}
+{{ "Entity:" | faint }}	{{ .Entity }}
+{{ "Code:" | faint }}	{{ .Code }}
+{{ "Description:" | faint }}	{{ .Description }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		gitmoji := gitmojiContainer.Gitmoji[index]
+		name := strings.Replace(strings.ToLower(gitmoji.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Gitmoji",
+		Items:     gitmojiContainer.Gitmoji,
+		Templates: templates,
+		Size:      12,
+		Searcher:  searcher,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("%s\n", gitmojiContainer.Gitmoji[i].Name)
+}
