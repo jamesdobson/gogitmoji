41e3a64a789a942f4f21b0f16b4093024ce09d2b
jamesdobson <dobson@softwarepunk.com>
2019-11-14 23:25:30 -0500

    🎨 Fixing ugly use of raw strings.

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	gitmoji, err := getGitmoji()
 
 	if err != nil {
-		log.Panic(`Unable to get list of gitmoji: `, err)
+		log.Panic("Unable to get list of gitmoji: ", err)
 	}
 
 	templates := &promptui.SelectTemplates{
@@ -67,15 +67,15 @@ func main() {
 	i, _, err := prompt.Run()
 
 	if err != nil {
-		log.Panic(`Prompt failed: `, err)
+		log.Panic("Prompt failed: ", err)
 	}
 
 	fmt.Printf("%s\n", gitmoji[i].Name)
 }
 
-const GitmojiURL string = `https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json`
-const GitmojiDirName string = `.gitmoji`
-const GitmojiFileName string = `gitmojis.json`
+const GitmojiURL string = "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json"
+const GitmojiDirName string = ".gitmoji"
+const GitmojiFileName string = "gitmojis.json"
 
 // Gets the gitmoji list from a local file cache if available;
 // otherwise, downloads the latest gitmoji list from github.com.
@@ -83,7 +83,7 @@ func getGitmoji() ([]Gitmoji, error) {
 	homedir, err := os.UserHomeDir()
 
 	if err != nil {
-		return nil, fmt.Errorf(`Cannot determine home directory: %v`, err)
+		return nil, fmt.Errorf("Cannot determine home directory: %v", err)
 	}
 
 	cacheFile := path.Join(homedir, GitmojiDirName, GitmojiFileName)
@@ -92,34 +92,34 @@ func getGitmoji() ([]Gitmoji, error) {
 
 	if err != nil {
 		if os.IsNotExist(err) {
-			log.Print(`Fetching list of gitmoji...`)
+			log.Print("Fetching list of gitmoji...")
 
 			r, err := http.Get(GitmojiURL)
 
 			if err != nil {
-				return nil, fmt.Errorf(`Unable to download gitmoji list: %v`, err)
+				return nil, fmt.Errorf("Unable to download gitmoji list: %v", err)
 			}
 
 			defer r.Body.Close()
 			content, err = ioutil.ReadAll(r.Body)
 
 			if err != nil {
-				return nil, fmt.Errorf(`Unable to download gitmoji list: %v`, err)
+				return nil, fmt.Errorf("Unable to download gitmoji list: %v", err)
 			}
 
 			err = os.MkdirAll(path.Dir(cacheFile), 0755)
 
 			if err != nil {
-				return nil, fmt.Errorf(`Unable to create gitmoji cache directory: %v`, err)
+				return nil, fmt.Errorf("Unable to create gitmoji cache directory: %v", err)
 			}
 
 			err = ioutil.WriteFile(cacheFile, content, 0644)
 
 			if err != nil {
-				return nil, fmt.Errorf(`Unable to write gitmoji cache: %v`, err)
+				return nil, fmt.Errorf("Unable to write gitmoji cache: %v", err)
 			}
 		} else {
-			return nil, fmt.Errorf(`Unable to read gitmoji cache: %v`, err)
+			return nil, fmt.Errorf("Unable to read gitmoji cache: %v", err)
 		}
 	}
 
@@ -127,7 +127,7 @@ func getGitmoji() ([]Gitmoji, error) {
 	err = json.Unmarshal([]byte(content), &gitmojiContainer)
 
 	if err != nil {
-		return nil, fmt.Errorf(`Cannot process gitmoji list; perhaps the file is corrupted? Underlying error: %v`, err)
+		return nil, fmt.Errorf("Cannot process gitmoji list; perhaps the file is corrupted? Underlying error: %v", err)
 	}
 
 	return gitmojiContainer.Gitmoji, nil
